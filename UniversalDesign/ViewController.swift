4029de6b192f3b97b9d3247e50a508d44118a57a
Mukesh <mmukesh.tecie@gmail.com>
2018-09-19 19:58:50 +0530

    Added universal design for iPhone and iPad

---
+++ UniversalDesign/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UniversalDesign
+//
+//  Created by Mukesh on 18/09/18.
+//  Copyright © 2018 Mukesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
